34d3c68f825cafeb88d6590e14b97fc53e5d598c
fabriziodee <124698914+fabriziodee@users.noreply.github.com>
2023-02-21 01:49:12 +0100

    Update _antiarabe.js

--- plugins/_antiarabe.js
+++ plugins/_antiarabe.js
@@ -5,14 +5,14 @@ handler.before = async function (m, {conn, isAdmin, isBotAdmin, isOwner } ) {
 	
 	if (!m.isGroup) return !1
 	let chat = global.db.data.chats[m.chat]
-	let te = `En este grupo solo se permite Numero fakes\n los siento sera expulsado del grupo`
+	let te = `In questo gruppo sono consentiti solo autorizzati\nMi dispiace che verrai espulso dal gruppo`
 	if (isBotAdmin && chat.onlyLatinos && !isAdmin && !isOwner) {
    if (m.sender.startsWith('212')) {
   //global.db.data.users[m.sender].banned = true
  m.reply(te, m.sender)
 conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
    } 
-   if (m.sender.startsWith('265')) {
+   if (m.sender.startsWith('92')) {
   m.reply(te, m.sender)
 conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
    } 
@@ -20,7 +20,7 @@ conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
   m.reply(te, m.sender)
 conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
    } 
-   if (m.sender.startsWith('258')) {
+   if (m.sender.startsWith('91')) {
   m.reply(te, m.sender)
 conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
    } 
