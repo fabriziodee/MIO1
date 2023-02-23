67d5be0fb02acb829232eb0d04b66e727f02691e
fabriziodee <124698914+fabriziodee@users.noreply.github.com>
2023-02-23 03:22:27 +0100

    Update info-groupofc.js

--- plugins/info-groupofc.js
+++ plugins/info-groupofc.js
@@ -2,43 +2,20 @@
 let handler = async (m, { conn }) => {
 
 m.reply(`
-*ʜᴏʟᴀ ᴇsᴛɪᴍᴀᴅᴏs ᴜsᴜᴀʀɪᴏs 👋🏻,  ᴛᴇ ɪɴᴠɪᴛᴏ ᴀ ᴜɴɪʀᴛᴇ ᴀ ʟᴏs ɢʀᴜᴘᴏs ᴏғɪᴄɪᴀʟᴇs ᴅᴇ ʟᴀs ғᴀᴍɪʟᴀs ᴛʜᴇ ʟᴏʟɪʙᴏᴛ-ᴍᴅ ʏ ɴᴏᴠᴀʙᴏᴛ-ᴍᴅ, ᴘᴀʀᴀ ᴄᴏɴᴠɪᴠɪʀ ᴄᴏɴ ʟᴀ ᴄᴏᴍᴜɴɪᴅᴀᴅ :ᴠ*
+*HEY CARO SFRUTTATORE DI SUPERFABRIBOT TI ELENCO IL GRUPPO UFFICIALE*
 
-*➤ ɢʀᴜᴘᴏs ᴏғɪᴄɪᴀʟᴇs ᴅᴇʟ ʙᴏᴛ:*
-1) https://chat.whatsapp.com/LZFE2MHvEkY7b69TmAGmHR
+*➤ GRUPPO UFFICIALE DEL BOT:*
+1) https://chat.whatsapp.com/BHZDSUDHuwWEed4Ut8aeUQ
 
-2) https://chat.whatsapp.com/IIteN3dd6rG87b2liETl2o
-
-ɢʀᴜᴘᴏ ᴅᴇʟ ᴄᴏʟᴀʙᴏʀᴀᴄɪᴏɴ ɢᴀᴛᴀʙᴏᴛ-ᴍᴅ ʏ ʟᴏʟɪʙᴏᴛ-ᴍᴅ 
-https://chat.whatsapp.com/CcJKHrxGBWAAH3PiBL07JS
-
-ɢʀᴜᴘᴏ ᴅᴇʟ ᴄᴏʟ ² (ᴀǫᴜɪ sᴇ ᴘᴇʀᴍɪᴛᴇ ᴛᴏᴅᴏ ʟᴏs ᴄᴏᴍᴀɴᴅᴏs ᴅᴇʟ ʙᴏᴛ)
-https://chat.whatsapp.com/FfiQUYDDtaG9bwaI7UKMAR
-
-ɪɴғᴏᴍᴀʀᴛᴇ sᴏʙʀᴇ ʟᴀs ɴᴜᴇᴠᴀs ᴀᴄᴛᴜᴀʟɪᴢᴀᴄɪᴏɴᴇs ᴅᴇʟ ʙᴏᴛ ᴀǫᴜɪ 
-https://chat.whatsapp.com/JESaesjOEcB6wnGX0QYT9o
-
-ɢʀᴜᴘᴏ ᴅᴇʟ ᴀʏᴜᴅᴀ sᴏʙʀᴇ ᴇʟ ʙᴏᴛ 
-https://chat.whatsapp.com/FRkr7jJHSJA5OjVtE64dDs
+2) https://chat.whatsapp.com/BHZDSUDHuwWEed4Ut8aeUQ
 
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-ɢʀᴜᴘᴏs ᴅᴇʟ ᴀᴍɪsᴛᴀᴅᴇs ᴅᴏɴᴅᴇ ᴇsᴛᴀ ᴇʟ ʙᴏᴛ
+*➤ GRUPPO SPAM:*
 
-*ஓீ͜ঔৣ͡🍒STICKER/𝑩Ø𝑻🔥ঔৣ͡ஓ*
-https://chat.whatsapp.com/DCJclB8oBAPIAoleUtNEaN
+https://chat.whatsapp.com/JiorKiJ2g3SDCt3Aln3hAO
 
-*꧁⁣𓆩Aᴹᴵᴳᴼˢ Cᴴᴬᵀˢ Y Aᴾᴼᴿᵀᴱ*
-https://chat.whatsapp.com/JZaD3sfNoVW4JvaoQ4uVwF
-
-*⇶⃤꙰𝑬𝒏𝒍𝒂𝒄𝒆 𝒍𝒐𝒍𝒊𝒃𝒐𝒕ꦿ⃟⃢*
-https://chat.whatsapp.com/DsFPvTLFDQaBroutCnD2DO
-ᴀǫᴜɪ ᴘᴜᴇᴅᴇ ᴍᴀɴᴅᴀ ᴛᴜ ᴇɴʟᴀᴄᴇ ʏ ᴛᴀᴍʙɪᴇɴ sɪ ǫᴜɪᴇʀᴇ ᴜɴ ʙᴏᴛ ᴘᴀʀᴀ ᴛᴜ ɢʀᴜᴘᴏ ᴘᴏɴᴇʀ ᴇʟ ᴄᴏᴍᴀɴᴅᴏ .ᴊᴏɪɴ (ᴍᴀs ᴇɴʟᴀᴄᴇ)
-
-*꧁⃢⃟҉💫🌺ঔঔ𝔼ℕ𝕃𝔸ℂ𝔼𝕊 𝕃𝔸 𝕁𝔼𝔽𝔸✨*
-https://chat.whatsapp.com/JegOp8NUSMd0vn75s4hkaj
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-
-sɪ ǫᴜɪᴇʀᴇs ǫᴜᴇ ᴛᴜ ɢʀᴜᴘᴏ ᴀᴘᴀʀᴇᴢᴄᴀɴ ᴀᴄᴀ ʜᴀʙʟᴀ ᴄᴏɴ ᴇʟ ᴀᴅᴍɪɴ ᴏғᴄ ᴅᴇʟ ʙᴏᴛ`)
+SE RITIENI POSSIBILE UNA COLLABORAZIONE NON ESITARE A CONTATTARE IL CREATORE CON IL COMANDO !owner`)
 
 }
 handler.help = ['support']
