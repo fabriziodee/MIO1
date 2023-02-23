d4133ddc4d9488c951f994a451bfd40243a6137f
fabriziodee <124698914+fabriziodee@users.noreply.github.com>
2023-02-23 05:08:01 +0100

    Update cmd-del.js

--- plugins/cmd-del.js
+++ plugins/cmd-del.js
@@ -3,11 +3,11 @@
 let handler = async (m, { text }) => {
     let hash = text
     if (m.quoted && m.quoted.fileSha256) hash = m.quoted.fileSha256.toString('hex')
-    if (!hash) throw `✳️ Ingrese el nombre del comamdo`
+    if (!hash) throw `✳️ Immettere il nome del comando`
     let sticker = global.db.data.sticker
-    if (sticker[hash] && sticker[hash].locked) throw '✳️ No puedes borrar este comando'
+    if (sticker[hash] && sticker[hash].locked) throw '✳️Non puoi eliminare questo comando'
     delete sticker[hash]
-    m.reply(`✅ Comando eliminado`)
+    m.reply(`✅ comando cancellato`)
 }
 
 
