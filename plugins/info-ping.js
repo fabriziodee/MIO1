4d1c98a737520c91114fa99415bf99c3ee285ff0
fabriziodee <124698914+fabriziodee@users.noreply.github.com>
2023-02-23 22:19:01 +0100

    Update info-ping.js

--- plugins/info-ping.js
+++ plugins/info-ping.js
@@ -17,8 +17,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
          exec(`neofetch --stdout`, (error, stdout, stderr) => {
           let child = stdout.toString("utf-8");
           let ssd = child.replace(/Memory:/, "Ram:");
-          m.reply(`${ssd}🚀 *AH MA FABRI HA LA FIBRA* : ${latensi.toFixed(4)} _ms_`
-          m.reply(`⌛*HEY FRA SONO ATTIVO DA*..  \n\n${muptime}`)  });
+          m.reply(`⌛*HEY FRA SONO ATTIVO DA*..  \n\n${muptime}`m.reply(`${ssd}🚀 *AH MA FABRI HA LA FIBRA* : ${latensi.toFixed(4)} _ms_`)  });
 }
 handler.help = ['ping']
 handler.tags = ['main']
