f7403dd6335f7e221c01a9d00a6b79858d43ec0d
fabriziodee <124698914+fabriziodee@users.noreply.github.com>
2023-02-23 04:39:09 +0100

    Update info-instalarbot.js

--- plugins/info-instalarbot.js
+++ plugins/info-instalarbot.js
@@ -7,28 +7,8 @@ let texto1 = `
 
 SE SARAI BUONO TI SPIEGHERO 
 USA IL COMANDO !owner PER CONTATTARMI 
-'
-let buttonMessage= {
-'document': { url: `https://youtu.be/13xBCq2ifGs` },
-'mimetype': `application/${document}`,
-'fileName': `𝑻𝒉𝒆 𝑳𝒐𝒍𝒊𝑩𝒐𝒕-𝑴𝑫`,
-'fileLength': 99999999999999,
-'pageCount': 200,
-'contextInfo': {
-'forwardingScore': 200,
-'isForwarded': true,
-'externalAdReply': {
-'mediaUrl': 'https://youtu.be/13xBCq2ifGs',
-'mediaType': 2,
-'previewType': 'pdf',
-'title': 'tutorial del instalación',
-'body': wm,
- }},
-'caption': texto1,
-'footer': wm,
-'buttons':[
-{buttonId: `${usedPrefix}menu`, buttonText: {displayText: '✳️ MENU ✳️'}, type: 1}],
-'headerType': 6 }
+
+
 conn.sendMessage(m.chat, buttonMessage, { quoted: m })}
 handler.command = /^(installabot|instalarbot)$/i
 handler.register = true
