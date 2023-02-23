d683cf819aaed54814f2b53323695f864bd51dea
fabriziodee <124698914+fabriziodee@users.noreply.github.com>
2023-02-23 04:39:56 +0100

    Update info-instalarbot.js

--- plugins/info-instalarbot.js
+++ plugins/info-instalarbot.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, usedPrefix }) => {
 let pp = 'https://i.imgur.com/lkch77g.jpeg' 
 var document = doc[Math.floor(Math.random() * doc.length)]    
 await m.reply(`╰⊱🔰⊱ *𝙸𝙽𝚂𝚃𝙰𝙻𝙰𝙲𝙸𝙾𝙽* ⊱🔰⊱╮`)
-  //conn.sendButton(m.chat, str, author, await(await fetch(pp)).buffer(), [['Menu', '/menu']], m)
+  
 let texto1 = `
 
 SE SARAI BUONO TI SPIEGHERO 
