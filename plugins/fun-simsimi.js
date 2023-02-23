e99dd377a34db0e561422876142808866b5d5d8b
fabriziodee <124698914+fabriziodee@users.noreply.github.com>
2023-02-23 09:08:18 +0100

    Update fun-simsimi.js

--- plugins/fun-simsimi.js
+++ plugins/fun-simsimi.js
@@ -6,8 +6,8 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
   if (!text) throw `hey👋 *${name}* Vuoi parlare un po con me? usa in questo modo \n\nEsempio : *${usedPrefix + command}* Ciao bot`
   m.react('🗣️') 
   await conn.sendPresenceUpdate('composing', m.chat)
-  //let res = await fetch(global.API('https://api.simsimi.net', '/v2/', { text: encodeURIComponent(m.text), lc: "italian" }, ''))
-  let res = await fetch(`https://api.simsimi.net/v2/?text=${text}&lc=italian`)
+  //let res = await fetch(global.API('https://api.simsimi.net', '/v2/', { text: encodeURIComponent(m.text), lc: "en" }, ''))
+  let res = await fetch(`https://api.simsimi.net/v2/?text=${text}&lc=en`)
   let json = await res.json()
   if (json.success) m.reply(json.success.replace('simsimi', 'DyLux').replace('Simsimi', 'DyLux').replace('sim simi', 'DyLux'))
   else throw json
