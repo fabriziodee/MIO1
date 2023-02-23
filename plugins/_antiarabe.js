6db78db19e23055073699fc268ad6a3f1b3b5c6a
fabriziodee <124698914+fabriziodee@users.noreply.github.com>
2023-02-23 02:48:56 +0100

    Update _antiarabe.js

--- plugins/_antiarabe.js
+++ plugins/_antiarabe.js
@@ -10,30 +10,37 @@ handler.before = async function (m, {conn, isAdmin, isBotAdmin, isOwner } ) {
    if (m.sender.startsWith('212')) {
   //global.db.data.users[m.sender].banned = true
  m.reply(te, m.sender)
+conn.groupParticipantsUpdate(m.chat, [m.sender], 'delete')	   
 conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
    } 
    if (m.sender.startsWith('92')) {
   m.reply(te, m.sender)
+conn.groupParticipantsUpdate(m.chat, [m.sender], 'delete')	   
 conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
    } 
    if (m.sender.startsWith('234')) {
   m.reply(te, m.sender)
+conn.groupParticipantsUpdate(m.chat, [m.sender], 'delete')	   
 conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
    } 
    if (m.sender.startsWith('91')) {
   m.reply(te, m.sender)
+conn.groupParticipantsUpdate(m.chat, [m.sender], 'delete')	   
 conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
    } 
    if (m.sender.startsWith('263')) {
   m.reply(te, m.sender)
+conn.groupParticipantsUpdate(m.chat, [m.sender], 'delete')	   
 conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
    } 
    if (m.sender.startsWith('967')) {
   m.reply(te, m.sender)
+conn.groupParticipantsUpdate(m.chat, [m.sender], 'delete')	   
 conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
    } 
    if (m.sender.startsWith('20')) {
   m.reply(te, m.sender)
+conn.groupParticipantsUpdate(m.chat, [m.sender], 'delete')	   
 conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
    } 
    
