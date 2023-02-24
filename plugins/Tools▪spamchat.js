62043e1b1e5e85fdeb1df900880e3fb252ef558e
fabriziodee <124698914+fabriziodee@users.noreply.github.com>
2023-02-24 15:24:12 +0100

    Create Tools▪spamchat.js

---
+++ plugins/Tools▪spamchat.js
@@ -0,0 +1,48 @@
+let handler = async(m, { conn, text }) => {
+
+    if (!text) return conn.reply(m.chat, '*_Inserisci il testo da inviare come spam!_*', m)
+
+    let pesan = `${text}`
+    await m.reply('*_START SPAM!_*\n\n*_Nota: il Bot invierà il messaggio 30 volte_*')
+
+await conn.reply(m.chat, (pesan), fakemsg)
+await conn.reply(m.chat, (pesan), fakemsg)
+await conn.reply(m.chat, (pesan), fakemsg)
+await conn.reply(m.chat, (pesan), fakemsg)
+await conn.reply(m.chat, (pesan), fakemsg)
+await conn.reply(m.chat, (pesan), fakemsg)
+await conn.reply(m.chat, (pesan), fakemsg)
+await conn.reply(m.chat, (pesan), fakemsg)
+await conn.reply(m.chat, (pesan), fakemsg)
+await conn.reply(m.chat, (pesan), fakemsg)
+await conn.reply(m.chat, (pesan), fakemsg)
+await conn.reply(m.chat, (pesan), fakemsg)
+await conn.reply(m.chat, (pesan), fakemsg)
+await conn.reply(m.chat, (pesan), fakemsg)
+await conn.reply(m.chat, (pesan), fakemsg)
+await conn.reply(m.chat, (pesan), fakemsg)
+await conn.reply(m.chat, (pesan), fakemsg)
+await conn.reply(m.chat, (pesan), fakemsg)
+await conn.reply(m.chat, (pesan), fakemsg)
+await conn.reply(m.chat, (pesan), fakemsg)
+await conn.reply(m.chat, (pesan), fakemsg)
+await conn.reply(m.chat, (pesan), fakemsg)
+await conn.reply(m.chat, (pesan), fakemsg)
+await conn.reply(m.chat, (pesan), fakemsg)
+await conn.reply(m.chat, (pesan), fakemsg)
+await conn.reply(m.chat, (pesan), fakemsg)
+await conn.reply(m.chat, (pesan), fakemsg)
+await conn.reply(m.chat, (pesan), fakemsg)
+await conn.reply(m.chat, (pesan), fakemsg)
+await conn.reply(m.chat, (pesan), fakemsg)
+
+}
+handler.help = ['spamchat'].map(v => v + ' *<texto>*')
+handler.tags = ['tools']
+handler.command = /^(spamchat)$/i
+
+handler.fail = null
+handler.exp = 100
+handler.limit = true
+
+export default handler
