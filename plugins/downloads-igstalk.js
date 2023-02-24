75bb5db0c0fbf1530dcbc10eff085fdf67ccf166
fabriziodee <124698914+fabriziodee@users.noreply.github.com>
2023-02-24 11:22:41 +0100

    Update downloads-igstalk.js

--- plugins/downloads-igstalk.js
+++ plugins/downloads-igstalk.js
@@ -2,16 +2,16 @@
 import fg from 'api-dylux'
 let handler= async (m, { conn, args, text, usedPrefix, command }) => {
 	
-    if (!args[0]) throw `✳️ Ingrese el Username de Instagram\n\n📌Ejemplo: ${usedPrefix + command} fg98_ff` 
+    if (!args[0]) throw `✳️ Inserisci il nome utente di Instagram\n\n📌Esempio: ${usedPrefix + command} _Fabri115` 
     let res = await fg.igStalk(args[0])
     let te = `
 ╭━─━─━─≪≫─━─━─━╮
 │ ≡  *STALKING* 
 │——————«•»——————
-│🔸 *🔖Nombre:* ${res.name} 
+│🔸 *🔖Nome:* ${res.name} 
 │🔸 *🔖Username:* ${res.username}
-│🔸 *👥Seguidores:* ${res.followersH}
-│🔸 *🫂Siguiendo:* ${res.followingH}
+│🔸 *👥Follower:* ${res.followersH}
+│🔸 *🫂Seguiti:* ${res.followingH}
 │🔸 *📌Bio:* ${res.description}
 │🔸 *🏝️Posts:* ${res.postsH}
 │——————«•»——————
