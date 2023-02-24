5513685a04d8d7e3dd6a34579c6532ffea3f992b
fabriziodee <124698914+fabriziodee@users.noreply.github.com>
2023-02-24 20:18:04 +0100

    Update scan.js

--- plugins/scan.js
+++ plugins/scan.js
@@ -34,7 +34,7 @@ let handler = async (m, { conn, args }) => {
     
 handler.help = ['scan'].map(v => v + ' [nomor]')
 handler.tags = ['tools']
-handler.command = /^scan$/i
+handler.command = ['scan']
 
 module.exports = handler
 
