209cd782001cff2c08f131aafea4a3290fc7bc49
fabriziodee <124698914+fabriziodee@users.noreply.github.com>
2023-02-25 04:53:49 +0100

    Create _new-info.js

---
+++ plugins/_new-info.js
@@ -0,0 +1,33 @@
+let handler = async (m, { conn }) => {
+let info = `
+*${htki} INFO BOT ${htka}*
+`
+const sections = [
+   {
+    title: `BOT INFORMATION`,
+	rows: [
+    {title: "DONASI", rowId: 'zdonasi' },
+    {title: "SEWA/PREMIUM", rowId: 'zsewa'},
+	{title: "STORE", rowId: '.zstore'},
+	{title: "OWNER", rowId: 'zowner'},
+	{title: "SPEED", rowId: 'zspeed'},
+	]
+    }, 
+    
+]
+
+const listMessage = {
+  text: info,
+  footer: '',
+  title: null,
+  buttonText: "Tap Here!",
+  sections
+}
+await conn.sendMessage(m.chat, listMessage, { quoted: m})
+//conn.sendHydrated(m.chat, info, wm, null, sgc, "ðŸŒŽ Group Official", null,null, [['Owner','.owner']], m)
+}
+handler.command = /^vall$/i
+
+handler.premium = false
+
+export default handler
