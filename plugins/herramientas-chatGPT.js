22e6c7ce86a62c8f6b9ce9578cddd7c50dc2493d
fabriziodee <124698914+fabriziodee@users.noreply.github.com>
2023-02-25 05:28:05 +0100

    Update herramientas-chatGPT.js

--- plugins/herramientas-chatGPT.js
+++ plugins/herramientas-chatGPT.js
@@ -1,5 +1,7 @@
 import fetch from 'node-fetch'
-let handler = async (m, { text }) => {
+let handler = async (m, { text, usedPrefix, command }) => {
+if (!text) throw `*❗FAMMI UNA DOMANDA O UNA RICHIESTA O DAMMI ORDINE*\n\n*—◉ ESEMPIO*\n*◉ ${usedPrefix + command} COSA PENSI SULL AMORE?*\n*◉ ${usedPrefix + command}*`
+try {
 m.reply('*⏳ CI STO PENSANDO ⏳*')
 await conn.sendPresenceUpdate('composing', m.chat)
 let tiores = await fetch(`https://api.lolhuman.xyz/api/openai?apikey=${lolkeysapi}&text=${text}&user=user-unique-id`)
@@ -8,5 +10,5 @@ m.reply(`${hasil.result}`.trim())
 } catch {
 throw `*[❗] PARLA ITALIANO IDIOTA!`
 }}
-handler.owner = true 
+handler.command = ['openai', 'ii', 'ia', 'io']
 export default handler
