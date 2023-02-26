e346737afaaf069928f29dd46ef9437f0398e8f4
fabriziodee <124698914+fabriziodee@users.noreply.github.com>
2023-02-26 12:19:09 +0100

    Update info-instalarbot.js

--- plugins/info-instalarbot.js
+++ plugins/info-instalarbot.js
@@ -1,52 +1,6 @@
 let handler = async (m, { conn, usedPrefix }) => { 
-await m.reply(`╰⊱🔰⊱ *ASPETTA* ⊱🔰⊱╮`)
-  //conn.sendButton(m.chat, str, author, await(await fetch(pp)).buffer(), [['Menu', '/menu']], m)
-let texto1 = `
-/
-------------------------------------
+await m.reply(`╰⊱🔰⊱ *AH SI, ASPETTA NE.. STO SCEMO* ⊱🔰⊱╮`)
 
-*—◉ ᴄᴏᴍᴀɴᴅᴏ ᴛᴇʀᴍᴜx*
->  |
->  |
->  |
->  |
->  |
->  |
->  |
->  |
->  |
->  |
->  |
->  |
-(|||)
-
-📌 |
-
-📌 |
-
- |
- |
-let buttonMessage= {
-'mimetype': `application/${document}`,
-'fileName': `-𝑴𝑫`,
-'fileLength': 99999999999999,
-'pageCount': 200,
-'contextInfo': {
-'forwardingScore': 200,
-'isForwarded': true,
-'externalAdReply': {
-'mediaUrl': 'https://youtu.be/!',
-'mediaType': 2,
-'previewType': 'pdf',
-'title': 'tutorial del instalación',
-'body': wm,
- }},
-'caption': texto1,
-'footer': wm,
-'buttons':[
-{buttonId: `${usedPrefix}menu`, buttonText: {displayText: '✳️ MENU ✳️'}, type: 1}],
-'headerType': 6 }
-conn.sendMessage(m.chat, buttonMessage, { quoted: m })}
 handler.command = /^(instalarbot|instalarbot)$/i
 handler.register = true
 export default handler
