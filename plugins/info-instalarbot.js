f0b765a033e2d5ba18c39edfefb0d9a21ad24903
fabriziodee <124698914+fabriziodee@users.noreply.github.com>
2023-02-26 12:19:56 +0100

    Update info-instalarbot.js

--- plugins/info-instalarbot.js
+++ plugins/info-instalarbot.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, usedPrefix }) => { 
-await m.reply(`╰⊱🔰⊱ *AH SI, ASPETTA NE.. STO SCEMO* ⊱🔰⊱╮`)
+let m.reply(`╰⊱🔰⊱ *AH SI, ASPETTA NE.. STO SCEMO* ⊱🔰⊱╮`)
 
 handler.command = /^(instalarbot|instalarbot)$/i
 handler.register = true
