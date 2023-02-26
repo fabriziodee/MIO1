1765ae8b1287b1df146ed6a02e27961acd6cccad
fabriziodee <124698914+fabriziodee@users.noreply.github.com>
2023-02-26 16:20:11 +0100

    Update game-fight.js

--- plugins/game-fight.js
+++ plugins/game-fight.js
@@ -3,35 +3,35 @@ let handler = async (m, { conn, usedPrefix, participants }) => {
   conn.fight = conn.fight ? conn.fight : {}
   const delay = time => new Promise(res => setTimeout(res, time));
 
-  if (typeof conn.fight[m.sender] != "undefined" && conn.fight[m.sender] == true) return m.reply(`Stai ancora combattendo.`)
+  if (typeof conn.fight[m.sender] != "undefined" && conn.fight[m.sender] == true) return m.reply(`Kamu masih bertarung.`)
 
   let users = participants.map(u => u.id)
-  var contro
-  contro = users[Math.floor(users.length * Math.random())]
-  while (typeof global.db.data.users[contro] == "undefined" || contro == m.sender) {
-    contro = users[Math.floor(users.length * Math.random())]
+  var lawan
+  lawan = users[Math.floor(users.length * Math.random())]
+  while (typeof global.db.data.users[lawan] == "undefined" || lawan == m.sender) {
+    lawan = users[Math.floor(users.length * Math.random())]
   }
 
-  let VecchiaSfida = getRandom(0, 1)
+  let lamaPertarungan = getRandom(0, 1)
 
-  m.reply(`*Voi* (level ${global.db.data.users[m.sender].level}) sfida *'@' +${conn.getName(contro)}* (level ${global.db.data.users[contro].level}) ed è in battaglia.\n\nAspetta ${VecchiaSfida} Un altro minuto e vediamo chi vince.`)
+  m.reply(`*Kamu* (level ${global.db.data.users[m.sender].level}) menantang *'@' +${conn.getName(lawan)}* (level ${global.db.data.users[lawan].level}) dan sedang dalam pertarungan.\n\nTunggu ${lamaPertarungan} menit lagi dan lihat siapa yg menang.`)
 
   conn.fight[m.sender] = true
 
-  await delay(1000 * 60 * VecchiaSfida)
+  await delay(1000 * 60 * lamaPertarungan)
 
-  let perdente = ['Nabbo', 'Cupo', 'non eccezionale', '', 'sconfitto']
-  let vincente = ['Grande', 'Pro', 'Master Game', 'Legenda game', 'Molto Pro', 'Spingere forte']
+  let alasanKalah = ['Noob', 'Cupu', 'Kurang hebat', 'Ampas kalahan', 'Gembel kalahan']
+  let alasanMenang = ['Hebat', 'Pro', 'Master Game', 'Legenda game', 'Sangat Pro', 'Rajin Nge-push']
 
-  let opportunità = []
-  for (let i = 0; i < global.db.data.users[m.sender].level; i++) opportunità.push(m.sender)
-  for (let i = 0; i < global.db.data.users[lawan].level; i++) opportunità.push(lawan)
+  let kesempatan = []
+  for (let i = 0; i < global.db.data.users[m.sender].level; i++) kesempatan.push(m.sender)
+  for (let i = 0; i < global.db.data.users[lawan].level; i++) kesempatan.push(lawan)
 
   let pointPemain = 0
   let pointLawan = 0
   for (let i = 0; i < 10; i++) {
-    let unggul = getRandom(0, opportunità.length - 1)
-    if (opportunità[unggul] == m.sender) pointPemain += 1
+    let unggul = getRandom(0, kesempatan.length - 1)
+    if (kesempatan[unggul] == m.sender) pointPemain += 1
     else pointLawan += 1
   }
 
@@ -39,14 +39,14 @@ let handler = async (m, { conn, usedPrefix, participants }) => {
     let hadiah = (pointPemain - pointLawan) * 10000
     global.db.data.users[m.sender].gold += hadiah
     global.db.data.users[m.sender].limit += 1
-    m.reply(`*${conn.getName(m.sender)}* [${pointPemain * 10}] - [${pointLawan * 10}] *${conn.getName(contro)}*\n\n*Voi* (level ${global.db.data.users[m.sender].level}) perdere contro *${conn.getName(contro)}* (level ${global.db.data.users[contro].level}) grazie a te ${vincente[getRandom(0, vincente.length - 1)]}\n\nRegalo . ${hadiah.toLocaleString()}\n+1 Limit`)
+    m.reply(`*${conn.getName(m.sender)}* [${pointPemain * 10}] - [${pointLawan * 10}] *${conn.getName(lawan)}*\n\n*Kamu* (level ${global.db.data.users[m.sender].level}) menang melawan *${conn.getName(lawan)}* (level ${global.db.data.users[lawan].level}) karena kamu ${alasanMenang[getRandom(0, alasanMenang.length - 1)]}\n\nHadiah . ${hadiah.toLocaleString()}\n+1 Limit`)
   } else if (pointPemain < pointLawan) {
     let denda = (pointLawan - pointPemain) * 100000
     global.db.data.users[m.sender].money -= denda
     global.db.data.users[m.sender].limit += 1
-    m.reply(`*${conn.getName(m.sender)}* [${pointPemain * 10}] - [${pointLawan * 10}] *${conn.getName(contro)}*\n\n*Voi* (level ${global.db.data.users[m.sender].level}) perdere contro *${conn.getName(contro)}* (level ${global.db.data.users[contro].level}) grazie a te ${perdente[getRandom(0, perdente.length - 1)]}\n\nIl tuo oro è ridotto ${denda.toLocaleString()}\n+1 Limit`)
+    m.reply(`*${conn.getName(m.sender)}* [${pointPemain * 10}] - [${pointLawan * 10}] *${conn.getName(lawan)}*\n\n*Kamu* (level ${global.db.data.users[m.sender].level}) kalah melawan *${conn.getName(lawan)}* (level ${global.db.data.users[lawan].level}) karena kamu ${alasanKalah[getRandom(0, alasanKalah.length - 1)]}\n\nGold kamu berkurang ${denda.toLocaleString()}\n+1 Limit`)
   } else {
-    m.reply(`*${conn.getName(m.sender)}* [${pointPemain * 10}] - [${pointLawan * 10}] *${conn.getName(contro)}*\n\nIl risultato è un pareggio, sorellina, non ottieni niente`)
+    m.reply(`*${conn.getName(m.sender)}* [${pointPemain * 10}] - [${pointLawan * 10}] *${conn.getName(lawan)}*\n\nHasil imbang kak, ga dapet apa apa`)
   }
 
   delete conn.fight[m.sender]
