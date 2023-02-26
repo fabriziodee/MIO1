58e0109ac4cbdafe59374957089cae9b185dd393
fabriziodee <124698914+fabriziodee@users.noreply.github.com>
2023-02-26 12:22:18 +0100

    Delete info-instalarbot.js

--- plugins/info-instalarbot.js
+++
@@ -1,6 +0,0 @@
-let handler = async (m, { conn, usedPrefix }) => { 
-m.reply(`╰⊱🔰⊱ *AH SI, ASPETTA NE.. STO SCEMO* ⊱🔰⊱╮`)
-
-handler.command = /^(instalarbot|instalarbot)$/i
-handler.register = true
-export default handler
