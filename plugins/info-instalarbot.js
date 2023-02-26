9d5ef81c21d1e4d8840a7f7b1adb0da78ae4333b
fabriziodee <124698914+fabriziodee@users.noreply.github.com>
2023-02-26 12:16:16 +0100

    Update info-instalarbot.js

--- plugins/info-instalarbot.js
+++ plugins/info-instalarbot.js
@@ -1,6 +1,56 @@
 let handler = async (m, { conn, usedPrefix }) => {
-await m.reply(`╰⊱🔰⊱ *ASPETTA* ⊱🔰⊱╮ *SE SARAI BUONO TI SPIEGHERO..USA IL COMANDO !owner PER CONTATTARMI*')
+let pp = 'https://i.imgur.com/lkch77g.jpeg' 
+var doc = ['pdf','zip','vnd.openxmlformats-officedocument.presentationml.presentation','vnd.openxmlformats-officedocument.spreadsheetml.sheet','vnd.openxmlformats-officedocument.wordprocessingml.document']
+var document = doc[Math.floor(Math.random() * doc.length)]    
+await m.reply(`╰⊱🔰⊱ *ASPETTA* ⊱🔰⊱╮`)
+  //conn.sendButton(m.chat, str, author, await(await fetch(pp)).buffer(), [['Menu', '/menu']], m)
+let texto1 = `
+/
+------------------------------------
 
-handler.command = /^(ib|instalarbot)$/i
+*—◉ ᴄᴏᴍᴀɴᴅᴏ ᴛᴇʀᴍᴜx*
+>  |
+>  |
+>  |
+>  |
+>  |
+>  |
+>  |
+>  |
+>  |
+>  |
+>  |
+>  |
+(|||)
+
+📌 |
+
+📌 |
+
+ |
+ |
+let buttonMessage= {
+'document': { url: `https://youtu.be/|` },
+'mimetype': `application/${document}`,
+'fileName': `-𝑴𝑫`,
+'fileLength': 99999999999999,
+'pageCount': 200,
+'contextInfo': {
+'forwardingScore': 200,
+'isForwarded': true,
+'externalAdReply': {
+'mediaUrl': 'https://youtu.be/!',
+'mediaType': 2,
+'previewType': 'pdf',
+'title': 'tutorial del instalación',
+'body': wm,
+ }},
+'caption': texto1,
+'footer': wm,
+'buttons':[
+{buttonId: `${usedPrefix}menu`, buttonText: {displayText: '✳️ MENU ✳️'}, type: 1}],
+'headerType': 6 }
+conn.sendMessage(m.chat, buttonMessage, { quoted: m })}
+handler.command = /^(instalarbot|instalarbot)$/i
 handler.register = true
 export default handler
