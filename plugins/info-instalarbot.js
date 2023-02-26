978b59ad140ddc297ecaa17105e882eb80bfc102
fabriziodee <124698914+fabriziodee@users.noreply.github.com>
2023-02-26 12:06:19 +0100

    Update info-instalarbot.js

--- plugins/info-instalarbot.js
+++ plugins/info-instalarbot.js
@@ -4,7 +4,7 @@ await m.reply(`╰⊱🔰⊱ *ASPETTA* ⊱🔰⊱╮`)
 let texto1 = 'SE SARAI BUONO TI SPIEGHERO..USA IL COMANDO !owner PER CONTATTARMI'
 
 
-conn.sendMessage(m.chat, buttonMessage, { quoted: m })}
+conn.sendMessage(m.chat, { quoted: m })}
 handler.command = ['install']
 handler.register = true
 export default handler
