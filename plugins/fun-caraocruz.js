a4b3ab8f74d61e12dee54724287689dd3f53d865
fabriziodee <124698914+fabriziodee@users.noreply.github.com>
2023-02-26 13:50:28 +0100

    Update fun-caraocruz.js

--- plugins/fun-caraocruz.js
+++ plugins/fun-caraocruz.js
@@ -4,11 +4,6 @@ let hasil = Math.floor(Math.random() * 50)
 let moneda = `*Moneta lanciata*
 Risultato: ${['testa', 'croce'].getRandom()} `.trim()
 
-conn.sendButton(m.chat, moneda, wm,  pp,
-[
-['Lancia di nuovo', `zlancia`]], m)}
-
-
 handler.tags = ['fun']
 handler.command = /^lancia|toc$/i
 //handler.exp = 0
