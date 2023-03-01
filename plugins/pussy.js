f98db60de2b75ec2a61b20ceefe745bdf0ea089d
fabriziodee <124698914+fabriziodee@users.noreply.github.com>
2023-03-01 15:54:03 +0100

    Update pussy.js

--- plugins/pussy.js
+++ plugins/pussy.js
@@ -1,12 +1,30 @@
-const axios = require('axios')
-let handler = async(m, { conn }) => {
-if (!DATABASE._data.chats[m.chat].nsfw && m.isGroup) throw '*[ ⚠️ ] I comandi +18 sono disabilitati in questo gruppo, se sei un amministratore di questo gruppo e vuoi abilitarli scrivi #enable nsfw*'
-let porn = await axios.get('https://meme-api.herokuapp.com/gimme/pussy')
-conn.sendFile(m.chat, `${porn.data.url}`, '', `${porn.data.title}`, m)
+import db from '../lib/database.js'
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, usedPrefix, command }) => {
+let name = await conn.getName(m.sender)
+	if (!global.db.data.chats[m.chat].nsfw && m.isGroup) throw `❗El grupo no admite contenido nsfw \n\n Para habilitar escriba \n*${usedPrefix}enable* nsfw`
+  var {age} = global.db.data.users[m.sender]
+   if (age <17) {
+   m.reply(`❎ Eres menor de edad! vuelve cuando tengas más de 18 años`)
+   throw false
+	}
+   
+   let pw = ["https://meme-api.herokuapp.com/gimme/pussy", "https://meme-api.herokuapp.com/gimme/LegalTeens"] 
+ let nk = pw[Math.floor(Math.random() * pw.length)]
+    let res = await fetch(nk)
+    if (!res.ok) throw await res.text()
+    let json = await res.json()
+    if (!json.url) throw '❎ Error'
+    await conn.reply(m.chat, global.wait, m)
+    conn.sendButton(m.chat, `_${command}_`.trim(), `*◈•${name}*`, json.url, [['「🔃 Sɪɢᴜɪᴇɴᴛᴇ 🔃」', `${usedPrefix + command }`]], fakemsg)
 }
 handler.help = ['pussy']
-handler.tags = ['images']
-handler.command = /^(pussy)$/i
-handler.fail = null
-handler.exp = 0
-module.exports = handler
+handler.tags = ['nsfw']
+handler.command = ['pussy']
+handler.limit = true
+handler.register = true
+
+
+
+export default handler
