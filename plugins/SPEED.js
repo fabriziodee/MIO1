f6b3ce3d10b3708f299e36d6579f413e8bcca451
fabriziodee <124698914+fabriziodee@users.noreply.github.com>
2023-03-02 18:33:58 +0100

    Update SPEED.js

--- plugins/SPEED.js
+++ plugins/SPEED.js
@@ -149,7 +149,7 @@ ${'```' + Object.keys(used).map((key, _, arr) => `${key.padEnd(Math.max(...arr.m
 handler.help = ['ping', 'speed']
 handler.tags = ['info', 'tools']
 
-handler.command = /^(ping|speed|info)$/i
+handler.command = /^(ping0|i)$/i
 export default handler
 
 const more = String.fromCharCode(8206)
