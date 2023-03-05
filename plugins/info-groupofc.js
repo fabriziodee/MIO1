4f805fbf04615af35d67d38c76223eb8c32a7aad
fabriziodee <124698914+fabriziodee@users.noreply.github.com>
2023-03-05 19:32:27 +0100

    Update info-groupofc.js

--- plugins/info-groupofc.js
+++ plugins/info-groupofc.js
@@ -4,16 +4,16 @@ let handler = async (m, { conn }) => {
 m.reply(`
 *HEY CARO SFRUTTATORE DI SUPERFABRIBOT TI ELENCO IL GRUPPO UFFICIALE*
 
-*➤ GRUPPO UFFICIALE DEL BOT: MoonClouds *
+*➤ GRUPPO UFFICIALE DEL BOT: 𝑴𝒐𝒐𝒏𝑪𝒍𝒐𝒖𝒅𝒔⟿ꪶ͢𝑴☁⃟ᬽ *
 
-1) https://chat.whatsapp.com/CICSIqMkjlhHHoiDsk2Vqw
+1) https://chat.whatsapp.com/K1IPit7Ceo3EkzV3QpQctQ
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-*➤ GRUPPO SPAM: MoonClouds *
+*➤ GRUPPO SPAM 𝑴𝒐𝒐𝒏𝑪𝒍𝒐𝒖𝒅𝒔 *
 
 2)https://chat.whatsapp.com/JiorKiJ2g3SDCt3Aln3hAO
 
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-SE RITIENI POSSIBILE UNA COLLABORAZIONE NON ESITARE A CONTATTARE IL CREATORE CON IL COMANDO !owner`)
+SE RITIENI POSSIBILE UNA COLLABORAZIONE NON ESITARE A CONTATTARE IL CREATORE CON (prefix)owner`)
 
 
 }
