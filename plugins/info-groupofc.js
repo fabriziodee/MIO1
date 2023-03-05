b83a6097c24b8ba12c5614463add5fd1aade98ae
fabriziodee <124698914+fabriziodee@users.noreply.github.com>
2023-03-05 19:45:46 +0100

    Update info-groupofc.js

--- plugins/info-groupofc.js
+++ plugins/info-groupofc.js
@@ -7,19 +7,19 @@ m.reply(`
 *➤ GRUPPO UFFICIALE DEL BOT: 𝑴𝒐𝒐𝒏𝑪𝒍𝒐𝒖𝒅𝒔⟿ꪶ͢𝑴☁⃟ᬽ *
 
 1) https://chat.whatsapp.com/K1lPit7Ceo3EkzV3QpQctQ
-┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┈┈┈┈┈┈┈┈┈┈┈┈
 *➤ GRUPPO SPAM 𝑴𝒐𝒐𝒏𝑪𝒍𝒐𝒖𝒅𝒔 *
 
 2)https://chat.whatsapp.com/JiorKiJ2g3SDCt3Aln3hAO
 
-┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┈┈┈┈┈┈┈┈┈┈┈┈
 SE RITIENI POSSIBILE UNA COLLABORAZIONE NON ESITARE A CONTATTARE IL CREATORE CON (prefix)owner`)
 
 
 }
 handler.help = ['support']
 handler.tags = ['main']
-handler.command = ['grupos', 'groupnova', 'dxgp', 'dygp', 'gpdynova', 'support'] 
+handler.command = ['grupos', 'groupnova', 'gp', 'dygp', 'gpdynova', 'support'] 
 handler.register = true
 
 export default handler
